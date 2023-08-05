b8248f8b093f90d96ce84f7dfaa061eeb0a6de7c
unknown <tarekyosry8@gmail.com>
2023-08-05 22:44:56 +0300

    Get data from Api

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import axios from "axios";
 
 function App() {
 
-  // const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=66fb58f8b9a599d14f93b88b7dc8876e'
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const api = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=66fb58f8b9a599d14f93b88b7dc8876e`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter'){
+      axios.get(api)
+      .then(response => {
+        setData(response.data)
+        console.log(response.data);
+      }) 
+      setLocation('')
+    }
+  }
+
 
   return (
     <div className="app">
+      <div className="search-box">
+        <input 
+        value={location}
+        placeholder="Search location"
+        onChange={(event) => setLocation(event.target.value)}
+        onKeyDown={searchLocation}
+        type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
